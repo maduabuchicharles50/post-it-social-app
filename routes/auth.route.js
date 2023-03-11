987396cc0ca3a435077f8069bbf133cb9bc6dc21
MbonuCharles <maduabuchicharles50@gmail.com>
2023-03-12 00:55:43 +0100

    login code chunk added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,27 +1,27 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
-//const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 //const jwt = require('jsonwebtoken')
-const { registerValidation } = require("../validation");
+//const { registerValidation } = require("../validation");
 
 // Register
 router.post("/register", async (req, res) => {
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  //const { error } = registerValidation(req.body);
+  //if (error) return res.status(400).send(error.details[0].message);
 
   //checking if user already exist
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist) return res.status(400).send("Email already exist");
+  //const emailExist = await User.findOne({ email: req.body.email });
+  //if (emailExist) return res.status(400).send("Email already exist");
 
   try {
-    //const salt = await bcrypt.genSalt(10);
-    //const hashpwd = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashpwd = await bcrypt.hash(req.body.password, salt);
 
     //created a new user
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashpwd,
     });
     const user = await newUser.save();
     res.status(201).json(user);
@@ -31,23 +31,17 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN FUNCTIONALITY
-// router.post("/login", async (req, res) => {
-//   try {
-//     const user = await User.findOne({ username: req.body.username });
-//     if (user) {
-//       const result = await bcrypt.compare(req.body.password, user.password);
-//       if (result) {
-//         const token = await jwt.sign({ username: user.username }, SECRET);
-//         return res.json({ token });
-//       } else {
-//         return res.status(400).json({ error: "password doesn't match" });
-//       }
-//     } else {
-//       return res.status(400).json({ error: "User doesn't exist" });
-//     }
-//   } catch (error) {
-//     return res.status(400).json({ error });
-//   }
-// });
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("wrong details");
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("wrong details");
+
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -1,23 +1,23 @@
-//JOI VALIDATION
-const joi = require("joi");
+// //JOI VALIDATION
+// const joi = require("joi");
 
-//Register Validation
-const registerValidation = (data) => {
-  const schema = {
-    username: joi.string().min(6).required(),
-    email: joi.string().min(6).required().email(),
-    password: joi.string().min(6).required(),
-  };
-  return joi.Validate(data, schema);
-};
+// //Register Validation
+// const registerValidation = (data) => {
+//   const schema = {
+//     username: joi.string().min(6).required(),
+//     email: joi.string().min(6).required().email(),
+//     password: joi.string().min(6).required(),
+//   };
+//   return joi.Validate(data, schema);
+// };
 
-const loginValidation = (data) => {
-  const schema = {
-    email: joi.string().min(6).required().email(),
-    password: joi.string().min(6).required(),
-  };
-  return joi.Validate(data, schema);
-};
+// const loginValidation = (data) => {
+//   const schema = {
+//     email: joi.string().min(6).required().email(),
+//     password: joi.string().min(6).required(),
+//   };
+//   return joi.Validate(data, schema);
+// };
 
-module.exports.registerValidation = registerValidation;
-module.exports.loginValidation = loginValidation;
+// module.exports.registerValidation = registerValidation;
+// module.exports.loginValidation = loginValidation;
