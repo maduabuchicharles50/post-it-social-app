501ad92798aa440004defd51fee098a269525629
MbonuCharles <maduabuchicharles50@gmail.com>
2023-03-13 07:30:47 +0100

    committed getAll comment

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -25,15 +25,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// // //GET ALL USERS
-// // router.get("/", async (req, res) => {
-// //   const comment = await Comment.find();
-// //   if (comment) {
-// //     //console.log(user);
-// //     return res.status(200).jsoncommen);
-// //   } else {
-// //     return res.status(500).json(err);
-// //   }
-// });
+//GET ALL USERS
+router.get("/", async (req, res) => {
+  const comment = await Comment.find();
+  if (comment) {
+    //console.log(user);
+    return res.status(200).json(comment);
+  } else {
+    return res.status(500).json(err);
+  }
+});
+
+// UPDATE COMMENT
+router.patch("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("update your account to continue");
+  }
+});
 
 module.exports = router;
